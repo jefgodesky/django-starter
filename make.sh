3f4162ab132b21b5c69a6ee67bcc7c6ed725d311
Jason Godesky <jefgodesky@gmail.com>
2023-09-29 13:21:11 -0400

    Create virtual environment for make script

--- make.sh
+++ make.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 cd src
+python3 -m venv env
+source env/bin/activate
 pip install -r requirements.dev.txt
+cd ..
 clear
 python make.py
+exit
+clear
+rm -Rf env
 rm -f make.py
 rm -- "$0"
