34ab4e848475a4825d034339c3711b69cead723d
Jason Godesky <jefgodesky@gmail.com>
2023-10-04 19:22:33 -0400

    Fix production deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,6 +14,7 @@ fi
 if [ "$DEBUG" = "1" ]; then
   python manage.py flush --no-input
   python manage.py migrate
+  exec "$@"
+else
+  exec gunicorn --bind 0.0.0.0:8000 --workers 1 --threads 1 --timeout 0 "$SITENAME.wsgi:application"
 fi
-
-exec "$@"
