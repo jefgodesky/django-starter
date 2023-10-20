0f10486ad76503da372e0d3c49a2abf0ee9070d8
Jason Godesky <jefgodesky@gmail.com>
2023-10-20 19:40:31 -0400

    Add make messages method: print_intro

--- make/messages_test.py
+++ make/messages_test.py
@@ -6,3 +6,17 @@ def test_print_bold(capsys):
     captured = capsys.readouterr()
     expected = "\033[1mHello, world!\033[0m\n"
     assert captured.out == expected
+
+
+def test_print_intro_bold_title(capsys):
+    messages.print_intro()
+    captured = capsys.readouterr()
+    expected = "\033[1mLet’s start a new Django project!\033[0m\n"
+    assert expected in captured.out
+
+
+def test_print_intro_text(capsys):
+    messages.print_intro()
+    captured = capsys.readouterr()
+    expected = "This script will help you get set up"
+    assert expected in captured.out
