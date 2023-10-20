b6ae0a4c73281225f08d969936ceeda810cd6c80
Jason Godesky <jefgodesky@gmail.com>
2023-10-20 10:29:05 -0400

    Fix number of blank lines around new settings

--- make.py
+++ make.py
@@ -138,10 +138,9 @@ def change_settings(filename: str, users: str):
     settings = change_database_settings(settings)
 
     new_settings_anchor = "from pathlib import Path"
-    new_settings_sep = os.linesep + os.linesep
     new_settings = [f'AUTH_USER_MODEL = "{users}.UserAccount"', "SITE_ID = 1"]
     new_settings_string = (
-        new_settings_sep + os.linesep.join(new_settings) + new_settings_sep
+        os.linesep + os.linesep + os.linesep.join(new_settings) + os.linesep
     )
 
     replacements = [
