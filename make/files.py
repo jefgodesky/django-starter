b74f8f7520533af891c1021d63c38c0a88df26e5
Jason Godesky <jefgodesky@gmail.com>
2023-10-21 15:19:43 -0400

    Add make files method: create_users_model_tests

--- make/files.py
+++ make/files.py
@@ -11,3 +11,30 @@ def exempt_long_lines(filename: str):
 
     with open(filename, "w") as file:
         file.writelines(modified_lines)
+
+
+def create_users_model_tests(users: str):
+    content = """import pytest
+import datetime
+from django.utils import timezone
+from .models import UserAccount
+
+
+@pytest.mark.django_db
+def test_active_default():
+    account = UserAccount()
+    assert account.is_active is True
+
+
+@pytest.mark.django_db
+def test_created_default():
+    before = timezone.now()
+    account = UserAccount()
+    after = timezone.now()
+    assert isinstance(account.created, datetime.datetime)
+    assert before <= account.created
+    assert after >= account.created
+"""
+
+    with open(f"./src/{users}/models_test.py", "w") as file:
+        file.write(content)
--- make/files_test.py
+++ make/files_test.py
@@ -8,28 +8,41 @@ import pytest
 def mock_file(monkeypatch):
     mock = mock_open()
     monkeypatch.setattr("builtins.open", mock)
-    return mock()
+    return mock
 
 
 def test_exempt_long_lines_lt(mock_file):
     content = "A" * 87 + "\n"
-    mock_file.readlines.return_value = [content]
+    mock_file().readlines.return_value = [content]
     files.exempt_long_lines("test.txt")
-    actual = mock_file.writelines.call_args[0][0]
+    actual = mock_file().writelines.call_args[0][0]
     assert actual == [content]
 
 
 def test_exempt_long_lines_eq(mock_file):
     content = "A" * 88 + "\n"
-    mock_file.readlines.return_value = [content]
+    mock_file().readlines.return_value = [content]
     files.exempt_long_lines("test.txt")
-    actual = mock_file.writelines.call_args[0][0]
+    actual = mock_file().writelines.call_args[0][0]
     assert actual == [content]
 
 
 def test_exempt_long_lines_gt(mock_file):
     content = "A" * 89 + "\n"
-    mock_file.readlines.return_value = [content]
+    mock_file().readlines.return_value = [content]
     files.exempt_long_lines("test.txt")
-    actual = mock_file.writelines.call_args[0][0]
+    actual = mock_file().writelines.call_args[0][0]
     assert actual == ["A" * 89 + "  # noqa: E501\n"]
+
+
+def test_create_users_model_tests_content(mock_file):
+    files.create_users_model_tests("users")
+    actual = mock_file().write.call_args[0][0]
+    assert "from .models import UserAccount" in actual
+
+
+def test_create_users_model_tests_filename(mock_file):
+    files.create_users_model_tests("users")
+    args = mock_file.call_args[0]
+    assert args[0] == "./src/users/models_test.py"
+    assert args[1] == "w"
