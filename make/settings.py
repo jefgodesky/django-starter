98e3140a3445fc732dcaabdcc610ec4eb8c67908
Jason Godesky <jefgodesky@gmail.com>
2023-10-21 13:03:00 -0400

    Add make settings method: change_database_settings

--- make/settings.py
+++ make/settings.py
@@ -12,3 +12,20 @@ def add_installed_apps(settings: str, users: str):
     apps_str = ",\n    ".join(apps)
     installed_str = f"INSTALLED_APPS = [\n    {apps_str},\n]"
     return settings.replace(match.group(0), installed_str)
+
+
+def change_database_settings(settings: str):
+    match_databases = re.search(r"DATABASES = {[^}]*}", settings, flags=re.DOTALL)
+    if not match_databases:
+        return settings
+    databases = """{
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}"""
+    return settings.replace(match_databases.group(0), f"DATABASES = {databases}")
--- make/settings_test.py
+++ make/settings_test.py
@@ -1,6 +1,9 @@
 import settings
 
-test_example = """INSTALLED_APPS = [
+test_example = """DATABASES = {
+}
+
+INSTALLED_APPS = [
     \"previously_installed\",
 ]"""
 
@@ -18,3 +21,18 @@ def test_add_installed_apps_adds_rest_framework():
 def test_add_installed_apps_adds_users():
     actual = settings.add_installed_apps(test_example, "users")
     assert '"users",' in actual
+
+
+def test_change_database_settings():
+    actual = settings.change_database_settings(test_example)
+    expected = """DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}"""
+    assert expected in actual
