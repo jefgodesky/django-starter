f40cf931d4866027add089285ed88d8f7e066944
Jason Godesky <jefgodesky@gmail.com>
2023-10-21 10:50:59 -0400

    Add make processes method: create_users_app

--- make/processes.py
+++ make/processes.py
@@ -3,3 +3,7 @@ import subprocess
 
 def create_django_project(project: str):
     subprocess.run(["django-admin", "startproject", project, "."], cwd="src")
+
+
+def create_users_app(users: str):
+    subprocess.run(["python", "manage.py", "startapp", users], cwd="src")
--- make/processes_test.py
+++ make/processes_test.py
@@ -26,3 +26,27 @@ def test_create_django_project_command(create_django_project_setup):
 def test_create_django_project_cwd(create_django_project_setup):
     run_args, _ = create_django_project_setup
     assert run_args[1]["cwd"] == "src"
+
+
+@pytest.fixture
+def create_users_app_setup(monkeypatch):
+    run_args = None
+
+    def mock_run(*args, **kwargs):
+        nonlocal run_args
+        run_args = (args, kwargs)
+
+    monkeypatch.setattr(subprocess, "run", mock_run)
+    test_project_name = "users"
+    processes.create_users_app(test_project_name)
+    return run_args, test_project_name
+
+
+def test_create_users_app_command(create_users_app_setup):
+    run_args, test_project_name = create_users_app_setup
+    assert run_args[0][0] == ["python", "manage.py", "startapp", "users"]
+
+
+def test_create_users_app_cwd(create_users_app_setup):
+    run_args, _ = create_users_app_setup
+    assert run_args[1]["cwd"] == "src"
